cd9585f8efd0fcac86de61035e5bb5d7656cfdc7
umlautslouder <138530689+umlautslouder@users.noreply.github.com>
2023-07-03 23:20:38 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+function generatePassword(length) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+// Prompt the user to enter the password length
+const length = parseInt(prompt("Enter the length of the password:"));
+
+// Generate the password
+const password = generatePassword(length);
+
+// Display the password
+console.log(`Generated Password: ${password}`);
+
